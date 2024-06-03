7c9ee5325f0ff8742b129116df1dca299e840cc9
joshua5301 <junha5301@gmail.com>
2024-06-04 08:27:34 +0900

    feat: add level restriction option

--- src/app.py
+++ src/app.py
@@ -16,11 +16,22 @@ problem_df = pd.read_csv(data_dir, index_col=0)
 st.header('백준 문제 추천해드립니다! :sunglasses:')
 
 handle = st.text_input(label='', placeholder='solved.ac 핸들을 입력해주세요.')
-if st.button('추천받기') and handle:
-    recommendations = collaborative_model.get_recommendations(handle, 10)
+left, mid, right = st.columns([1, 10, 1])
+with right.popover('설정 :gear:'):
+    tiers = ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Ruby']
+    levels = [f'{tier} {num}' for tier in tiers for num in range(5, 0, -1)]
+    min_level, max_level = st.select_slider('티어 제한', options=levels, value=('Bronze 5', 'Ruby 1'))
+if left.button('추천받기') and handle:
+    matched_ids = []
+    id_num = 10
+    while len(matched_ids) < 10:
+        ids = collaborative_model.get_recommendations(handle, id_num)
+        is_in_range = lambda id: levels.index(min_level) + 1 <= problem_df.loc[id]['level'] <= levels.index(max_level) + 1
+        matched_ids = [id for id in ids if is_in_range(id)]
+        id_num *= 2
     row1 = st.columns(5)
     row2 = st.columns(5)
-    for id, col in zip(recommendations, row1 + row2):
+    for id, col in zip(matched_ids, row1 + row2):
         title = problem_df.loc[id]['titleKo']
         level = problem_df.loc[id]['level']
         tile = col.container(border=True)
