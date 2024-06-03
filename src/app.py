2797b1671709c902cfbbe03b2cbf1230bf190fc6
joshua5301 <junha5301@gmail.com>
2024-06-04 07:22:24 +0900

    style: change double colons into single colons

--- src/app.py
+++ src/app.py
@@ -5,18 +5,18 @@ import os
 
 # 페이지 기본 설정
 st.set_page_config(
-    page_title = "백준 알고리즘 문제 추천 시스템",
-    layout = "wide"
+    page_title = '백준 알고리즘 문제 추천 시스템',
+    layout = 'wide'
 )
 
 project_dir = os.path.dirname(os.path.dirname(__file__))
 data_dir = os.path.join(project_dir, 'data', 'preprocessed', 'problem_info.csv')
 problem_df = pd.read_csv(data_dir, index_col=0)
 
-st.header("백준 문제 추천해드립니다! :sunglasses:")
+st.header('백준 문제 추천해드립니다! :sunglasses:')
 
 handle = st.text_input(label='', placeholder='solved.ac 핸들을 입력해주세요.')
-if st.button("추천받기") and handle:
+if st.button('추천받기') and handle:
     recommendations = collaborative_model.get_recommendations(handle, 10)
     row1 = st.columns(5)
     row2 = st.columns(5)
