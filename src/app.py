b0ce338f1edb7956c7de8980cc4117e0ef4e3b2b
joshua5301 <junha5301@gmail.com>
2024-06-03 21:05:13 +0900

    feat: add complete website

--- src/app.py
+++ src/app.py
@@ -1,22 +1,36 @@
 import streamlit as st
 import pandas as pd 
-from collaborative_model import *
+import collaborative_model
+import os
 
-
-#페이지 기본 설정
+# 페이지 기본 설정
 st.set_page_config(
     page_title = "백준 알고리즘 문제 추천 시스템",
     layout = "wide"
 )
 
-#페이지 헤더, 서브 헤더 제목 설정
-return_list = list()
-st.header("협업 기반 필터링 추천 제공")
-if st.button("추천받기"):
-    buttons = get_recommendations('37aster', 10)
-    for button in buttons: 
-            st.link_button(f"Go to {button}",f"https://www.acmicpc.net/problem/{button}")
+project_dir = os.path.dirname(os.path.dirname(__file__))
+data_dir = os.path.join(project_dir, 'data', 'preprocessed', 'problem_info.csv')
+problem_df = pd.read_csv(data_dir, index_col=0)
 
+st.header("백준 문제 추천해드립니다!")
 
-else:
-    st.write("(추천 받기)를 클릭하세요")
+handle = st.text_input(label='', placeholder='solved.ac 핸들을 입력해주세요.')
+if st.button("추천받기") and handle:
+    recommendations = collaborative_model.get_recommendations(handle, 10)
+    row1 = st.columns(5)
+    row2 = st.columns(5)
+    for id, col in zip(recommendations, row1 + row2):
+        title = problem_df.loc[id]['titleKo']
+        level = problem_df.loc[id]['level']
+        with col:
+            image_dir = os.path.join(project_dir, 'assets', f'{level}.png')
+            st.image(image_dir, width=100)
+            hide_img_fs = '''
+            <style>
+            button[title="View fullscreen"]{
+                visibility: hidden;}
+            </style>
+            '''
+            st.markdown(hide_img_fs, unsafe_allow_html=True)
+            st.link_button(f"{id} - {title}",f"https://www.acmicpc.net/problem/{id}")
