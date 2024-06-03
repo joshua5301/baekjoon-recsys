e856b4fffecc02e470292fee82be3a2c332a123c
joshua5301 <junha5301@gmail.com>
2024-06-03 15:40:02 +0900

    feat: add simple model

---
+++ src/model.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+import pandas as pd
+from scipy.sparse import csr_matrix
+import implicit
+
+def get_recommendations(handle: str, num: int) -> list[int]:
+    """Get recommendations by using simple collaborative filitering.
+
+    Parameters
+    ----------
+    handle : str
+        User that recommendations will be given
+    num : int
+        Number of recommendations
+
+    Returns
+    -------
+    list[int]
+        List of recommendations
+    """
+    project_dir = os.path.dirname(os.path.dirname(__file__))
+    data_dir = os.path.join(project_dir, 'data', 'preprocessed', 'solved_info.csv')
+    solved_df = pd.read_csv(data_dir, index_col=0)
+
+    handle_to_id = {}
+    for id, user in enumerate(set(solved_df['handle'])):
+        handle_to_id[user] = id
+
+    user_item_matrix = np.zeros((len(set(solved_df['handle'])), 32000), dtype=np.int32)
+    for handle, problem in solved_df.itertuples(index=False):
+        id = handle_to_id[handle]
+        user_item_matrix[id][problem] = 1
+    user_item_matrix = csr_matrix(user_item_matrix)
+
+    model = implicit.als.AlternatingLeastSquares(factors=64, regularization=0.05, alpha=2.0)
+    model.fit(user_item_matrix)
+
+    my_id = handle_to_id['37aster']
+    items, _ = model.recommend(my_id, user_item_matrix[my_id], N=num)
+    return list(items)
+
+if __name__ == '__main__':
+    problems = get_recommendations(handle='37aster', num=10)
+    print(problems)
